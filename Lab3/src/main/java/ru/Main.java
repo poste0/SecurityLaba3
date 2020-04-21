20a2429763491f84288be347137b8fa48303dc63
poste0 <79372009578@yandex.ru>
2020-04-21 13:35:49 +0400

    Laba3

--- Lab3/src/main/java/ru/Main.java
+++ Lab3/src/main/java/ru/Main.java
@@ -1,7 +1,61 @@
 package ru;
 
+import java.io.*;
+import java.util.List;
+import java.util.Map;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        File file = new File("torrent.torrent");
+
+        byte[] bytes = new byte[(int) file.length()];
+        BufferedInputStream in = new BufferedInputStream(new FileInputStream(file));
+
+        BDecoder decoder = new BDecoder();
+        Map dict = decoder.decodeStream(in);
+        dict.forEach(mapConsumer);
 
     }
+
+    private static BiConsumer mapConsumer = new BiConsumer() {
+        @Override
+        public void accept(Object o, Object o2) {
+            if (o2 instanceof Map){
+                Map m = (Map) o2;
+                m.forEach(this);
+            }
+            else if(o2 instanceof List) {
+                System.out.println(o);
+                ((List) o2).forEach(listConsumer);
+            }
+            else if(o2 instanceof byte[]){
+                String o2String = new String((byte[]) o2);
+                System.out.println(o + " " + o2String);
+            }
+            else{
+                System.out.println(o + " " + o2);
+            }
+
+        }
+    };
+
+    private static Consumer listConsumer = new Consumer() {
+        @Override
+        public void accept(Object o) {
+            if(o instanceof List){
+                ((List) o).forEach(this);
+            }
+            else if(o instanceof Map){
+                ((Map) o).forEach(mapConsumer);
+            }
+            else if(o instanceof byte[]){
+                System.out.println(new String((byte[]) o));
+            }
+            else{
+                System.out.println(o);
+            }
+        }
+    };
 }
---
+++ Lab3/src/main/java/ru/TorrentUtils.java
@@ -0,0 +1,46 @@
+/*******************************************************************************
+ * TorrentUtils.java
+ *
+ * Copyright (c) 2012 SeedBoxer Team.
+ *
+ * This file is part of SeedBoxer.
+ *
+ * SeedBoxer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SeedBoxer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with SeedBoxer.  If not, see <http ://www.gnu.org/licenses/>.
+ ******************************************************************************/
+package ru;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Map;
+
+public class TorrentUtils {
+
+	@SuppressWarnings("rawtypes")
+	public static String getTorrentName(File torrent) throws IOException {
+		FileInputStream is = new FileInputStream(torrent);
+		BufferedInputStream bufferedInputStream = new BufferedInputStream(is);
+		try {
+			Map bdecode = BDecoder.decode(bufferedInputStream);
+
+			Map map = (Map) bdecode.get("info");
+			return new String((byte[]) map.get("name"));
+		} finally {
+			bufferedInputStream.close();
+			is.close();
+		}
+	}
+
+}
